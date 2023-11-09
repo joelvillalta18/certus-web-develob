389ba37d254b49abb2e577d9ff352870aa453e13
joel villalta <giancarlos64a@gmail.com>
2023-11-09 08:38:50 -0500

    cambios hoy

---
+++ index.js
@@ -0,0 +1,8 @@
+// console.log(8);
+// console.log('8');
+const mensaje = 'Sumador';
+const number1 = +prompt('Número 1');
+const number2 = +prompt('Número 2');
+
+document.getElementById('message').innerHTML = mensaje;
+document.getElementById('result').innerHTML = number1 + number2;

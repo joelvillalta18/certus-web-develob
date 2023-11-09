4a8dcda99334679a3a0f6df10ba62dfe98472b38
joel villalta <giancarlos64a@gmail.com>
2023-11-09 08:44:29 -0500

    cambios hoy

---
+++ data/data.js
@@ -0,0 +1,54 @@
+export const socialNetworks = [
+  {
+    id: 1,
+    href: 'https://www.youtube.com/@elliotgaramendi',
+    title: 'Red social de YouTube',
+    content: '<i class="bi bi-youtube"></i>'
+  },
+  {
+    id: 2,
+    href: 'https://github.com/elliotgaramendi',
+    title: 'Red social de GitHub',
+    content: '<i class="bi bi-github"></i>'
+  },
+  {
+    id: 3,
+    href: 'https://www.linkedin.com/in/elliotgaramendi/',
+    title: 'Red social de LinkedIn',
+    content: '<i class="bi bi-linkedin"></i>'
+  },
+  {
+    id: 4,
+    href: 'https://www.instagram.com/elliotgaramendi/',
+    title: 'Red social de Instagram',
+    content: '<i class="bi bi-instagram"></i>'
+  }
+];
+
+  
+  export const teams = [
+    {
+      id: 1,
+      title: 'H07',
+      subtitle: 'Sede Norte',
+      description: 'LENGUAJES PARA DES Y SOP SIST (202320-10184-H07-TE)',
+      image: 'https://www.certus.edu.pe/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2019/01/sede-norte-min.jpg.webp',
+      slug: './teams/h07'
+    },
+    {
+      id: 2,
+      title: 'H10',
+      subtitle: 'Sede Norte',
+      description: 'LENGUAJES PARA DES Y SOP SIST (202320-10184-H10-TE)',
+      image: 'https://www.certus.edu.pe/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2019/01/sede-norte-min.jpg.webp',
+      slug: './teams/h10'
+    },
+    {
+      id: 3,
+      title: 'P28',
+      subtitle: 'Sede Villa el Slavador',
+      description: 'LENGUAJES PARA DES Y SOP SIST (202320-10184-P28-TE)',
+      image: 'https://www.certus.edu.pe/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2019/01/sede-villaelsalvador-min.jpg.webp',
+      slug: './teams/p28'
+    }
+  ];
--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-// console.log(8);
-// console.log('8');
-const mensaje = 'Sumador';
-const number1 = +prompt('Número 1');
-const number2 = +prompt('Número 2');
+import { socialNetworks, teams } from './data/data.js';
 
-document.getElementById('message').innerHTML = mensaje;
-document.getElementById('result').innerHTML = number1 + number2;
+const socialNetworksList = document.getElementById('socialNetworksList');
+const teamsList = document.getElementById('teamsList');
+
+socialNetworks.forEach(element => {
+  const { id, href, title, content } = element;
+  socialNetworksList.innerHTML += `
+    <li key="${id}">
+      <a
+        href="${href}"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="link"
+        title="${title}"
+      >
+        ${content}
+      </a>
+    </li>
+  `;
+});
+
+teams.forEach(element => {
+  const { id, title, subtitle, description, image, slug } = element;
+  teamsList.innerHTML += `
+    <li
+      class="card"
+      key="${id}"
+    >
+      <img
+        src="${image}"
+        alt="${title}" class="img img--card" width="256" height="256" />
+      <div class="card__body">
+        <h3 class="card__title">${title}</h3>
+        <h4 class="card__subtitle">${subtitle}</h4>
+        <p class="card__text">${description}</p>
+        <a href="${slug}" class="button">Proyectos</a>
+      </div>
+    </li>
+  `
+})
